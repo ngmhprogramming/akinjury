f442543f6b5cd2ba3f131e673b389798c089e4d5
ngmhprogramming <ngmhcartoon@gmail.com>
2019-11-22 15:21:47 +0800

    basic web pages and code

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "/uploads"
+ALLOWED_EXTENSIONS = set(["jpg", "jpeg", "png", "gif"])
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+	return render_template("chat.html")
+
+@app.route("/identify", methods=["GET", "POST"])
+def identify():
+	upload = request.files["file"]
+	return render_template("identify.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
