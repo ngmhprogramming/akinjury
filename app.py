7ce27c7b17b7ff6cc808e147b59a7b6e1e72e184
ngmhprogramming <ngmhcartoon@gmail.com>
2019-11-22 16:18:32 +0800

    basic session system for chat

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug.utils import secure_filename
+from os import urandom
+from binascii import b2a_hex
 
 app = Flask(__name__)
+app.secret_key = "chennuodeceoofsex"
 
 UPLOAD_FOLDER = "/uploads"
 ALLOWED_EXTENSIONS = set(["jpg", "jpeg", "png", "gif"])
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+def get_session_id():
+    if "id" in session: return session["id"]
+    return None
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -14,9 +21,19 @@ def allowed_file(filename):
 def index():
 	return render_template("index.html")
 
-@app.route("/chat")
+@app.route("/chat", methods=["GET", "POST"])
 def chat():
-	return render_template("chat.html")
+	if request.method == "GET":
+		session_id = get_session_id()
+		if session_id is None: session["id"] = b2a_hex(urandom(64))
+		session_id = get_session_id()
+		print(session_id)
+		return render_template("chat.html")
+	else:
+		session_id = get_session_id()
+		if session_id is not None:
+			session.pop("id", None)
+		return redirect(url_for("index"))
 
 @app.route("/message", methods=["POST"])
 def message():
